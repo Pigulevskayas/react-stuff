1b83b1c371b8d88f0b8b03f0ce177dc1cac980dd
Alexandra <alexandrapigulevskaya@gmail.com>
2019-12-26 16:38:48 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Iconbar } from "./iconbar";
 
 
 ReactDOM.render(
-	<Iconbar orientation='vertical' />, document.getElementById('root')
+	<Iconbar orientation='horizontal' />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
